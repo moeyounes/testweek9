3b7d1c5a74580ba59ee4870b883b10968f645afb
Mohamed Ben Younes <mohamedbenyounes@Mohameds-MacBook-Pro-2.local>
2018-02-22 22:24:10 -0800

    first

---
+++ js/main.js
@@ -0,0 +1,37 @@
+
+const moe= document.getElementById('test');
+const kiv= document.createElement('h1');
+kiv.textContent="allllo";
+console.log(kiv);
+console.log(moe);
+moe.appendChild(kiv);
+
+
+
+
+
+//
+// const div = document.querySelector(".one.two").style.backgroundColor = "red";
+// const newDIV = document.createElement("DIV");
+// document.body.appendChild(newDIV)
+// newDiv.id = "temp";
+// const newH1 = document.createElement("H1");
+// newH1.textContent = "sometext";
+// newDiv.appendChild(newH1);
+// const newDiv2 = document.createElement("DIV");
+// newDiv2.id = "alsotemp";
+// const newerH1 = document.createElement("H1");
+// const newestH1 = document.createTextNode("some more text");
+// newerH1.appendChild(newestH1);
+// newDiv2.appendChild(newerH1);
+//
+// function swapAttr() {
+//   let imageTag = document.querySelector("IMG");
+//   if (imageTag.alt === "lion") {
+//     imageTag.alt = "tiger";
+//     imageTag.src = "img/tiger.jpg";
+//   } else {
+//     imageTag.alt = "lion";
+//     imageTag.src = "img/lion.jpg";
+//   }
+// }
